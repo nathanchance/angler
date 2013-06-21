850114e1e7c5620c342a22b5100a1a35cd0e6816
Sahara <keun-o.park@windriver.com>
2013-06-21 11:12:28 +0900

    PM / QoS: Add pm_qos_update_target/flags tracepoints
    This patch adds tracepoints to pm_qos_update_target and
    pm_qos_update_flags. It's useful for checking pm qos action,
    previous value and current value.
    
    Signed-off-by: Sahara <keun-o.park@windriver.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -48,6 +48,7 @@
 
 #include <linux/uaccess.h>
 #include <linux/export.h>
+#include <trace/events/power.h>
 
 /*
  * locking rule: all changes to constraints or notifiers lists
@@ -320,6 +321,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
+	trace_pm_qos_update_target(action, prev_value, curr_value);
 	/*
 	 * if cpu mask bits are set, call the notifier call chain
 	 * to update the new qos restriction for the cores
@@ -395,6 +397,7 @@ bool pm_qos_update_flags(struct pm_qos_flags *pqf,
 
 	spin_unlock_irqrestore(&pm_qos_lock, irqflags);
 
+	trace_pm_qos_update_flags(action, prev_value, curr_value);
 	return prev_value != curr_value;
 }
 
