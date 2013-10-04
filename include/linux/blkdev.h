7dc121f93fd7ae7f03fd3d0eff0e4f1ce16555a1
Christoph Hellwig <hch@infradead.org>
2013-10-04 06:49:11 -0700

    block: remove request ref_count
    This reference count has been around since before git history, but the only
    place where it's used is in blk_execute_rq, and ther it is entirely useless
    as it is incremented before submitting the request and decremented in the
    end_io handler before waking up the submitter thread.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -164,8 +164,6 @@ struct request {
 
 	unsigned short ioprio;
 
-	int ref_count;
-
 	void *special;		/* opaque pointer available for LLD use */
 	char *buffer;		/* kaddr of the current segment if available */
 
