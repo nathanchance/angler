50d2d611f25dea28be1266e83bae42b85631b7c9
Eric Dumazet <edumazet@google.com>
2013-12-12 15:41:56 -0800

    pkt_sched: set root qdisc before change() in attach_default_qdiscs()
    After commit 95dc19299f74 ("pkt_sched: give visibility to mq slave
    qdiscs") we call disc_list_add() while the device qdisc might be
    the noop_qdisc one.
    
    This shows up as duplicates in "tc qdisc show", as all inactive devices
    point to noop_qdisc.
    
    Fix this by setting dev->qdisc to the new qdisc before calling
    ops->change() in attach_default_qdiscs()
    
    Add a WARN_ON_ONCE() to catch any future similar problem.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_api.c
+++ net/sched/sch_api.c
@@ -221,8 +221,11 @@ static struct Qdisc *qdisc_match_from_root(struct Qdisc *root, u32 handle)
 
 static void qdisc_list_add(struct Qdisc *q)
 {
+	struct Qdisc *root = qdisc_dev(q)->qdisc;
+
+	WARN_ON_ONCE(root == &noop_qdisc);
 	if ((q->parent != TC_H_ROOT) && !(q->flags & TCQ_F_INGRESS))
-		list_add_tail(&q->list, &qdisc_dev(q)->qdisc->list);
+		list_add_tail(&q->list, &root->list);
 }
 
 void qdisc_list_del(struct Qdisc *q)
