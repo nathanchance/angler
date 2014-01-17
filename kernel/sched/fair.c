fea3b16845a7e431943d07c1c8fac988d90f1de5
Daniel Lezcano <daniel.lezcano@linaro.org>
2014-01-17 10:04:01 +0100

    sched: Remove 'cpu' parameter from idle_balance()
    The cpu parameter passed to idle_balance() is not needed as it could
    be retrieved from 'struct rq.'
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: alex.shi@linaro.org
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1389949444-14821-1-git-send-email-daniel.lezcano@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7705,7 +7705,7 @@ out:
  * idle_balance is called by schedule() if this_cpu is about to become
  * idle. Attempts to pull tasks from other CPUs.
  */
-void idle_balance(int this_cpu, struct rq *this_rq)
+void idle_balance(struct rq *this_rq)
 {
 	struct sched_domain *sd;
 	int pulled_task = 0;
@@ -7714,6 +7714,7 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 	int min_power = INT_MAX;
 	int balance_cpu = -1;
 	struct rq *balance_rq = NULL;
+	int this_cpu = this_rq->cpu;
 
 	this_rq->idle_stamp = this_rq->clock;
 
