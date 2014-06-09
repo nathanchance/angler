3e3dd8472ce153d2f98ade70ca86130b7e197335
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-06-09 08:24:17 -0700

    rcu: Loosen __call_rcu()'s rcu_head alignment constraint
    The m68k architecture aligns only to 16-bit boundaries, which can cause
    the align-to-32-bits check in __call_rcu() to trigger.  Because there is
    currently no known potential need for more than one low-order bit, this
    commit loosens the check to 16-bit boundaries.
    
    Reported-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Lai Jiangshan <laijs@cn.fujitsu.com>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -2382,7 +2382,7 @@ __call_rcu(struct rcu_head *head, void (*func)(struct rcu_head *rcu),
 	unsigned long flags;
 	struct rcu_data *rdp;
 
-	WARN_ON_ONCE((unsigned long)head & 0x3); /* Misaligned rcu_head! */
+	WARN_ON_ONCE((unsigned long)head & 0x1); /* Misaligned rcu_head! */
 	debug_rcu_head_queue(head);
 	head->func = func;
 	head->next = NULL;
