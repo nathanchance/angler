3113927727fe4318e5586a4bc2588dd23ee4c2a2
David Howells <dhowells@redhat.com>
2014-07-18 18:56:34 +0100

    KEYS: Call ->free_preparse() even after ->preparse() returns an error
    Call the ->free_preparse() key type op even after ->preparse() returns an
    error as it does cleaning up type stuff.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Steve Dickson <steved@redhat.com>
    Acked-by: Jeff Layton <jlayton@primarydata.com>
    Reviewed-by: Sage Weil <sage@redhat.com>

--- security/keys/key.c
+++ security/keys/key.c
@@ -492,7 +492,7 @@ int key_instantiate_and_link(struct key *key,
 		ret = __key_link_begin(keyring, key->type, key->description,
 				       &prealloc);
 		if (ret < 0)
-			goto error_free_preparse;
+			goto error;
 	}
 
 	ret = __key_instantiate_and_link(key, &prep, keyring, authkey,
@@ -501,10 +501,9 @@ int key_instantiate_and_link(struct key *key,
 	if (keyring)
 		__key_link_end(keyring, key->type, prealloc);
 
-error_free_preparse:
+error:
 	if (key->type->preparse)
 		key->type->free_preparse(&prep);
-error:
 	return ret;
 }
 
@@ -817,7 +816,7 @@ key_ref_t key_create_or_update(key_ref_t keyring_ref,
 		ret = ktype->preparse(&prep);
 		if (ret < 0) {
 			key_ref = ERR_PTR(ret);
-			goto error_put_type;
+			goto error_free_prep;
 		}
 		if (!description)
 			description = prep.description;
@@ -952,9 +951,9 @@ int key_update(key_ref_t key_ref, const void *payload, size_t plen)
 
 	up_write(&key->sem);
 
+error:
 	if (key->type->preparse)
 		key->type->free_preparse(&prep);
-error:
 	return ret;
 }
 EXPORT_SYMBOL(key_update);
