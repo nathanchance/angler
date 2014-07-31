62985b9d7c85ec334813c312a430adaa8f4d66a1
Tim Chen <tim.c.chen@linux.intel.com>
2014-07-31 10:29:48 -0700

    sched: Add function single_task_running to let a task check if it is the only task running on a cpu
    This function will help an async task processing batched jobs from
    workqueue decide if it wants to keep processing on more chunks of batched
    work that can be delayed, or to accumulate more work for more efficient
    batched processing later.
    
    If no other tasks are running on the cpu, the batching process can take
    advantgae of the available cpu cycles to a make decision to continue
    processing the existing accumulated work to minimize delay,
    otherwise it will yield.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -160,6 +160,7 @@ extern int nr_threads;
 DECLARE_PER_CPU(unsigned long, process_counts);
 extern int nr_processes(void);
 extern unsigned long nr_running(void);
+extern bool single_task_running(void);
 extern unsigned long nr_iowait(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
