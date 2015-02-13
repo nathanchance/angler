0ee9ebbae1bbf5b22bacf6d934c5ca002fdd639d
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-02-13 14:36:00 -0800

    lib/bitmap.c: elide bitmap_copy_le on little-endian
    On little-endian, there's no reason to have an extra, presumably less
    efficient, way of copying a bitmap.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/bitmap.c
+++ lib/bitmap.c
@@ -1106,6 +1106,7 @@ EXPORT_SYMBOL(bitmap_allocate_region);
  *
  * Require nbits % BITS_PER_LONG == 0.
  */
+#ifdef __BIG_ENDIAN
 void bitmap_copy_le(unsigned long *dst, const unsigned long *src, unsigned int nbits)
 {
 	unsigned int i;
@@ -1118,3 +1119,4 @@ void bitmap_copy_le(unsigned long *dst, const unsigned long *src, unsigned int n
 	}
 }
 EXPORT_SYMBOL(bitmap_copy_le);
+#endif
