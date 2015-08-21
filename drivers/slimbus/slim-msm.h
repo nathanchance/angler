de7ea781af384106460cba0e222397e4be20aabc
Sagar Dharia <sdharia@codeaurora.org>
2015-08-21 17:54:12 -0600

    slimbus: ngd: Optimize bulk write transactions
    Allocate bulk-write transactions in advance for most-commonly
    used bulk sizes, and reallocate only if size greater than that is
    requested by client.
    
    Change-Id: Ib5a80f3713fe5b94bd6dcf45ffe61e498fada192
    Signed-off-by: Sagar Dharia <sdharia@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/slimbus/slim-msm.h
+++ drivers/slimbus/slim-msm.h
@@ -231,9 +231,10 @@ struct msm_slim_pdata {
 };
 
 struct msm_slim_bulk_wr {
-	phys_addr_t	phys;
+	dma_addr_t	wr_dma;
 	void		*base;
 	int		size;
+	int		buf_sz;
 	int		(*cb)(void *ctx, int err);
 	void		*ctx;
 	bool		in_progress;
