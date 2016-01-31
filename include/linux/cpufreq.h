2fb70c35d55b9699f66a3a1c08e365ebcdbad9bf
ZaneZam <cyxman@yahoo.com>
2016-01-31 14:08:36 +0100

    cpufreq: add ZZMoove big.LITTLE Edition (bLE) development version as of 09.01.2016
    Modified for Angler by anarkia1976
    
    working features in this first version:
    
    - zzmoove freq scaling method (without limit functionality)
    - proportional scaling (all modes)
    - fast scaling up/down with afs (fully functional)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -438,6 +438,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
