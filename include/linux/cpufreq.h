571c681b6f6cac2f29ace863d26c09ceef626245
Pranav Vashi <neobuddy89@gmail.com>
2016-04-30 22:48:36 +0530

    cpufreq: Add latest impulse governor
    * Removed sched stuff.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -461,6 +461,9 @@ extern struct cpufreq_governor cpufreq_gov_relaxed;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
