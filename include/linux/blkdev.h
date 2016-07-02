c2b0811fd78bcf4a17bf858dbf436699fd688d6f
Joe Maples <joe@frap129.org>
2016-07-02 17:35:54 +0000

    zen-tune: Increase BLKDEV_MAX_RQ to 32
    Signed-off-by: Joe Maples <joe@frap129.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -37,7 +37,7 @@ struct blkcg_gq;
 
 #define BLKDEV_MIN_RQ	4
 #ifdef CONFIG_ZEN_INTERACTIVE
-#define BLKDEV_MAX_RQ	16
+#define BLKDEV_MAX_RQ	32
 #else
 #define BLKDEV_MAX_RQ	128	/* Default maximum */
 #endif
