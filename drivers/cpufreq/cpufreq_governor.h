791830678b3565c0a138e7fc9a91990a88f20849
Joe Maples <joe@frap129.org>
2016-07-10 17:26:44 +0000

    cpufreq: Add Chill cpu gov
    Signed-off-by: Joe Maples <joe@frap129.org>
    [@nathanchance: added default governor header in cpufreq_governor.h and updated for dwork]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -182,8 +182,10 @@ struct cs_dbs_tuners {
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
 	unsigned int down_threshold;
+	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
 	unsigned int touch_load_duration;
+	unsigned int sleep_depth;
 };
 
 /* Common Governor data across policies */
