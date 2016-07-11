774c41504fed7cb4fcb4af471f35260f58304d1b
Joe Maples <joe@frap129.org>
2016-07-11 22:41:29 +0000

    cpufreq: chill: Add boost option
    Jumps to max frequency after increasing frequency n times, where n is the value of boost_count.
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -203,6 +203,8 @@ struct cs_dbs_tuners {
 	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
 	unsigned int sleep_depth;
+	unsigned int boost_enabled;
+	unsigned int boost_count;
 };
 
 struct ex_dbs_tuners {
