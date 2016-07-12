245c6a7436e3b73afe71cbc72b9569e45ca440e5
Adrian Salido <salidoa@google.com>
2016-07-12 13:31:17 -0700

    msm: mdss: change retire fence signaling
    Signal retire fence on first vsync after frame has been programmed to
    hardware. This indicates when the frame starts being fetched by
    MDSS hardware.
    
    Change-Id: Ib69f64ff17a1609b67ff34ef277f658bfa5cfb5a
    Signed-off-by: Adrian Salido <salidoa@google.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1962,7 +1962,6 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON);
 
-	__vsync_set_vsync_handler(mfd);
 	__validate_and_set_roi(mfd, data);
 
 	if (ctl->ops.wait_pingpong && mdp5_data->mdata->serialize_wait4pp)
@@ -2000,6 +1999,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 			&commit_cb);
 		ATRACE_END("display_commit");
 	}
+	__vsync_set_vsync_handler(mfd);
 
 	if (!mdp5_data->kickoff_released)
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_CTX_DONE);
