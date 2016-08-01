e1ffbb61089c87c3aad393ab15b126aa03a466f9
andip71 <andreasp@gmx.de>
2016-08-01 11:37:29 +0200

    printk: Add sys kernel interface to configure linux printk logging
    - now logging can be switched on and off via /sys/kernel/printk_mode interface
    - default is 1 (on)
    - Enabled time stamp information in logs again

--- kernel/printk.c
+++ kernel/printk.c
@@ -48,6 +48,8 @@
 
 #include <asm/uaccess.h>
 
+#include "printk_interface.h"
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1930,6 +1932,10 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+		return 0;
+
 	return vprintk_emit(0, -1, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
@@ -1974,6 +1980,10 @@ asmlinkage int printk(const char *fmt, ...)
 {
 	va_list args;
 	int r;
+
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+		return 0;
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
