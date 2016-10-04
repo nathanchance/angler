1906cc4620dc4c9fc709bbda86a65325462daec9
Stratos Karafotis <stratosk@semaphore.gr>
2016-10-04 23:22:16 +0300

    cpufreq: Introduce touch boost tunables to ondemand and conservative
    ondemand:
    - touch_load: the virtual load that governor "sees" when there is a touch
    	in the screen (default value 70).
    - touch_load_duration: the duration in ms that the touch_load is effective
    	after the touch (default value 1000ms).
    - touch_load_threshold: the real load threshold. Loads greater than this
    	threshold will trigger the touch boost (default value 10).
    
    conservative
    - touch_load_duration: the duration of touch boost (default value 1000ms).
    	When there is a touch in the screen the freq_step tunable will be
    	double if there governor must increase the frequency.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -192,6 +192,9 @@ struct od_dbs_tuners {
 	unsigned int up_threshold;
 	unsigned int powersave_bias;
 	unsigned int io_is_busy;
+	unsigned int touch_load;
+	unsigned int touch_load_duration;
+	unsigned int touch_load_threshold;
 };
 
 struct cs_dbs_tuners {
@@ -202,6 +205,7 @@ struct cs_dbs_tuners {
 	unsigned int down_threshold;
 	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
+	unsigned int touch_load_duration;
 	unsigned int sleep_depth;
 	unsigned int boost_enabled;
 	unsigned int boost_count;
@@ -336,6 +340,7 @@ static ssize_t show_sampling_rate_min_gov_pol				\
 }
 
 extern struct mutex cpufreq_governor_lock;
+extern unsigned long touch_jiffies;
 
 void dbs_check_cpu(struct dbs_data *dbs_data, int cpu);
 bool need_load_eval(struct cpu_dbs_common_info *cdbs,
