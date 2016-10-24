6fde1b1f62f533fb96b29c2b04f95bea3dfe958f
Nathan Chancellor <natechancellor@gmail.com>
2016-10-24 14:59:53 -0700

    drivers: cpufreq: add Flash governor
    This is currently just a rebranded interactive governor, I am just laying the foundation for future changes.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -476,6 +476,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_FLASH)
+extern struct cpufreq_governor cpufreq_gov_flash;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_flash)
 #endif
 
 /*********************************************************************
