77f56ca1e947df2d84b088d41dabace1444926d5
Joe Maples <joe@frap129.org>
2016-11-02 22:01:57 +0000

    uksm: Fix warning
    Signed-off-by: Joe Maples <joe@frap129.org>

--- mm/uksm.c
+++ mm/uksm.c
@@ -2672,7 +2672,7 @@ out:
  * to the next pass of ksmd - consider, for example, how ksmd might be
  * in cmp_and_merge_page on one of the rmap_items we would be removing.
  */
-inline int unmerge_uksm_pages(struct vm_area_struct *vma,
+static inline int unmerge_uksm_pages(struct vm_area_struct *vma,
 		      unsigned long start, unsigned long end)
 {
 	unsigned long addr;
