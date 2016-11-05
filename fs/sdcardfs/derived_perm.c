472b04262ca27ec910e563b4cdbc3aa4a57ab22f
Nathan Chancellor <natechancellor@gmail.com>
2016-11-05 02:42:21 -0700

    sdcardfs: change d_u.d_child to d_child
    d_u.d_child was switched to just d_child in commit "move d_rcu from overlapping d_child to overlapping d_alias"
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- fs/sdcardfs/derived_perm.c
+++ fs/sdcardfs/derived_perm.c
@@ -150,7 +150,7 @@ void fixup_perms_recursive(struct dentry *dentry, const char* name, size_t len) 
 		}
 	} else 	if (descendant_may_need_fixup(info->perm)) {
 		mutex_lock(&dentry->d_inode->i_mutex);
-		list_for_each_entry(child, &dentry->d_subdirs, d_u.d_child) {
+		list_for_each_entry(child, &dentry->d_subdirs, d_child) {
 				fixup_perms_recursive(child, name, len);
 		}
 		mutex_unlock(&dentry->d_inode->i_mutex);
