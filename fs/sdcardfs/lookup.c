bdf8ef07870de13751fc83798c020a26a0444610
Nathan Chancellor <natechancellor@gmail.com>
2016-12-03 22:19:46 -0700

    [SQUASH] Revert sdcardfs commits
    commit 7ac74c95cd7df9fdddedac01baad5a444ee93037
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:16:35 2016 -0700
    
        Revert "sdcardfs: Truncate packages_gid.list on overflow"
    
        This reverts commit 0edee1486570f971b3d888aad346936d324c95bd.
    
    commit a07b51157eb26fa287595e054f63983e879c3c44
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:17:10 2016 -0700
    
        Revert "sdcardfs: change d_u.d_child to d_child"
    
        This reverts commit 3e853d033bec2d035f5840d18b0aa79bbd8ab521.
    
    commit 8949b8da5629c099585ae939ccbe981190c66373
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:18:36 2016 -0700
    
        Revert "ANDROID: sdcardfs: fix itnull.cocci warnings"
    
        This reverts commit cab989f0e36053af77a140defe60b56de692acd4.
    
    commit 25149554410b43405eb17bf5e706b096513199de
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:18:50 2016 -0700
    
        Revert "sdcardfs: Added top to sdcardfs_inode_info"
    
        This reverts commit 0c674f9ec07ffd4a27f171a24d82e7b09f5293ad.
    
    commit a066f7bf68b626b782750b3d9485491b55f43dec
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:18:56 2016 -0700
    
        Revert "sdcardfs: Fix locking"
    
        This reverts commit 0dda4079c4402c4d449f678f0a049447bfc956d2.
    
    commit 6939ee381f68f286a69fed9f7c44260fea4dfa94
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:19:12 2016 -0700
    
        Revert "sdcardfs: Check for other cases on path lookup"
    
        This reverts commit fca4cb4fbcdd7f81cb268e2ccb0d183b15c8128e.
    
    commit 95ce935e1770e4be964a70d960be701a08954a21
    Author: Nathan Chancellor <natechancellor@gmail.com>
    Date:   Sat Dec 3 22:19:19 2016 -0700
    
        Revert "sdcardfs: override umask on mkdir and create"
    
        This reverts commit 4c2d3677f1fe01d6bd265539d6ddd526f6aa45a0.

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -236,31 +236,10 @@ static struct dentry *__sdcardfs_lookup(struct dentry *dentry,
 	/* now start the actual lookup procedure */
 	lower_dir_dentry = lower_parent_path->dentry;
 	lower_dir_mnt = lower_parent_path->mnt;
+
 	/* Use vfs_path_lookup to check if the dentry exists or not */
 	err = vfs_path_lookup(lower_dir_dentry, lower_dir_mnt, name, 0,
 				&lower_path);
-	/* check for other cases */
-	if (err == -ENOENT) {
-		struct dentry *child;
-		struct dentry *match = NULL;
-		spin_lock(&lower_dir_dentry->d_lock);
-		list_for_each_entry(child, &lower_dir_dentry->d_subdirs, d_child) {
-			if (child && child->d_inode) {
-				if (strcasecmp(child->d_name.name, name)==0) {
-					match = dget(child);
-					break;
-				}
-			}
-		}
-		spin_unlock(&lower_dir_dentry->d_lock);
-		if (match) {
-			err = vfs_path_lookup(lower_dir_dentry,
-						lower_dir_mnt,
-						match->d_name.name, 0,
-						&lower_path);
-			dput(match);
-		}
-	}
 
 	/* no error: handle positive dentries */
 	if (!err) {
@@ -386,9 +365,11 @@ struct dentry *sdcardfs_lookup(struct inode *dir, struct dentry *dentry,
 	if (dentry->d_inode) {
 		fsstack_copy_attr_times(dentry->d_inode,
 					sdcardfs_lower_inode(dentry->d_inode));
-		/* get derived permission */
+		/* get drived permission */
+		mutex_lock(&dentry->d_inode->i_mutex);
 		get_derived_permission(parent, dentry);
 		fix_derived_permission(dentry->d_inode);
+		mutex_unlock(&dentry->d_inode->i_mutex);
 	}
 	/* update parent directory's atime */
 	fsstack_copy_attr_atime(parent->d_inode,
