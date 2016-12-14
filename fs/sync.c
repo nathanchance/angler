80444a6c352ea9714e30152425269e3388862984
Francisco Franco <franciscofranco.1990@gmail.com>
2016-12-14 21:10:33 +0000

    fs: sync: if we're blocking fsync then we should also avoid getting the file descriptor, avoiding some overhead
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -158,12 +158,14 @@ void emergency_sync(void)
  */
 SYSCALL_DEFINE1(syncfs, int, fd)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	struct super_block *sb;
 	int ret;
 
 	if (!fsync_enabled)
 		return 0;
+
+	f = fdget(fd);
 
 	if (!f.file)
 		return -EBADF;
@@ -226,11 +228,13 @@ EXPORT_SYMBOL(vfs_fsync);
 
 static int do_fsync(unsigned int fd, int datasync)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	int ret = -EBADF;
 
 	if (!fsync_enabled)
 		return 0;
+
+	f = fdget(fd);
 
 	if (f.file) {
 		ret = vfs_fsync(f.file, datasync);
