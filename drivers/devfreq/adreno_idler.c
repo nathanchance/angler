c71a6f45fd737a2573b72c6ecba0de006b7a4e7d
Nathan Chancellor <natechancellor@gmail.com>
2016-12-20 22:24:01 -0700

    adreno_idler: fix a declaration after statement warning
    I should just disable these but whatever.
    
    drivers/devfreq/adreno_idler.c: In function 'adreno_idler':
    drivers/devfreq/adreno_idler.c:72:2: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
      bool display_on = is_display_on();
      ^~~~
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -65,11 +65,11 @@ static unsigned int idlecount = 0;
 int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq)
 {
-	if (!adreno_idler_active)
-		return 0;
-
 	/* Boolean to let us know if the display is on*/
 	bool display_on = is_display_on();
+
+	if (!adreno_idler_active)
+		return 0;
 
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
