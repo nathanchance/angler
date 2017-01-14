d050b6a2590a1cd5eb6c814c4ec68f812a726565
Pranav Vashi <neobuddy89@gmail.com>
2017-01-15 04:07:03 +0530

    msm: use %*pb[l] to print bitmaps including cpumasks and nodemasks
    printk and friends can now format bitmaps using '%*pb[l]'.  cpumask
    and nodemask also provide cpumask_pr_args() and nodemask_pr_args()
    respectively which can be used to generate the two printf arguments
    necessary to format the specified cpu/nodemask.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -107,8 +107,8 @@ static void init_watchdog_work(struct work_struct *work);
 static void dump_cpu_alive_mask(struct msm_watchdog_data *wdog_dd)
 {
 	static char alive_mask_buf[MASK_SIZE];
-	cpulist_scnprintf(alive_mask_buf, MASK_SIZE,
-						&wdog_dd->alive_mask);
+	scnprintf(alive_mask_buf, MASK_SIZE, "%*pbl",
+					cpumask_pr_args(&wdog_dd->alive_mask));
 	printk(KERN_INFO "cpu alive mask from last pet %s\n", alive_mask_buf);
 }
 
