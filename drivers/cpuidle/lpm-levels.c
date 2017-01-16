dd2eedae68ea9b3ebdc788e1fd74339b4b822492
Josh Chasky <jchasky@gmail.com>
2017-01-16 11:29:24 -0700

    [SQUASHED] Revert some cpuidle commits
    Josh discovered these would lock his system down right at boot and I have been having reports of random reboots; let's just get rid of these, we aren't missing much.
    
    Revert "cpuidle: lpm-levels: Remove local_irq_enable from lpm_cpuidle_select"
    This reverts commit a03332b1ef1f37e2844330aa0d4a25916534fa67.
    
    Revert "cpuidle: lpm_levels: Remove duplicate cpuidle tracepoints"
    This reverts commit 49fd5c5ce49b0c1a546579d596873cb0c7652e35.
    
    Revert "lpm-levels: Check for need_resched just before entering PSCI"
    This reverts commit a191fd9ee77358d090d23ad44d8808c74d8e2b67.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -690,9 +690,12 @@ static int lpm_cpuidle_select(struct cpuidle_driver *drv,
 
 	idx = cpu_power_select(dev, cluster->cpu);
 
-	if (idx < 0)
+	if (idx < 0) {
+		local_irq_enable();
 		return -EPERM;
+	}
 
+	trace_cpu_idle_rcuidle(idx, dev->cpu);
 	return idx;
 }
 
@@ -705,6 +708,11 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 	int64_t start_time = ktime_to_ns(ktime_get()), end_time;
 	struct power_params *pwr_params;
 
+	if (need_resched()) {
+		dev->last_residency = 0;
+		goto exit;
+	}
+
 	pwr_params = &cluster->cpu->levels[idx].pwr;
 	sched_set_cpu_cstate(smp_processor_id(), idx + 1,
 		pwr_params->energy_overhead, pwr_params->latency_us);
@@ -716,9 +724,6 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 	trace_cpu_idle_enter(idx);
 	lpm_stats_cpu_enter(idx);
 
-	if (need_resched() || (idx < 0))
-		goto exit;
-
 	if (idx > 0)
 		update_debug_pc_event(CPU_ENTER, idx, 0xdeaffeed, 0xdeaffeed,
 					true);
@@ -728,13 +733,15 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 		update_debug_pc_event(CPU_EXIT, idx, success, 0xdeaffeed,
 					true);
 
-exit:
 	lpm_stats_cpu_exit(idx, success);
 	cluster_unprepare(cluster, cpumask, idx, true);
 	cpu_unprepare(cluster, idx, true);
 
 	sched_set_cpu_cstate(smp_processor_id(), 0, 0, 0);
 	trace_cpu_idle_exit(idx, success);
+
+exit:
+	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
 	end_time = ktime_to_ns(ktime_get()) - start_time;
 	dev->last_residency = do_div(end_time, 1000);
 	local_irq_enable();
