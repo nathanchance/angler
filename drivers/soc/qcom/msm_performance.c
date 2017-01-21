331569c4ba5c62d11b358d3d1f92d9b247703376
Nathan Chancellor <natechancellor@gmail.com>
2017-01-20 17:42:39 -0700

    Revert msm_hotplug
    Thank goodness...
    
    Revert "flash_defconfig: enable MSM_HOTPLUG"
    This reverts commit b4a3a35342d277c047cd7feecab5ae0ece9bb3b2.
    
    Revert "soc: qcom: msm_hotplug v2.4"
    This reverts commit b5cd4e4973978f2fae75efea9b62d750c4856809.
    
    Revert "soc: qcom: msm_hotplug v2.2"
    This reverts commit 5ee1792a2ad192d8f286b27c7f6a521063c51b11.
    
    Revert "soc: qcom: msm_hotplug v2.11"
    This reverts commit 7db57a2273243b1d2c244dfc0b115034d6cbc44c.
    
    Revert "soc: qcom: msm_hotplug v2.1"
    This reverts commit c264a43c2c48dc6d40dc99697fcb38990f7db317.
    
    Revert "soc: qcom: msm_hotplug v2"
    This reverts commit 01516e01e480ab69efdc556aa53bffe8c410887f.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -171,7 +171,6 @@ device_param_cb(num_clusters, &param_ops_num_clusters, NULL, 0644);
 
 static int set_max_cpus(const char *buf, const struct kernel_param *kp)
 {
-#if 0
 	unsigned int i, ntokens = 0;
 	const char *cp = buf;
 	int val;
@@ -202,7 +201,6 @@ static int set_max_cpus(const char *buf, const struct kernel_param *kp)
 	}
 
 	schedule_delayed_work(&evaluate_hotplug_work, 0);
-#endif
 
 	return 0;
 }
@@ -1011,7 +1009,6 @@ device_param_cb(iowait_ceiling_pct, &param_ops_iowait_ceiling_pct, NULL, 0644);
 
 static int set_workload_detect(const char *buf, const struct kernel_param *kp)
 {
-#if 0
 	unsigned int val, i;
 	struct cluster *i_cl;
 	unsigned long flags;
@@ -1053,8 +1050,6 @@ static int set_workload_detect(const char *buf, const struct kernel_param *kp)
 	}
 
 	wake_up_process(notify_thread);
-#endif
-
 	return 0;
 }
 
