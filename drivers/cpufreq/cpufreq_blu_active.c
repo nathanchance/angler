97b0fee401260e7e2d2439e28d2ff754dd81ae33
jollaman999 <admin@jollaman999.com>
2017-01-22 17:10:28 +0900

    cpufreq: blu_active: Revert "Use sched_get_cpus_busy() to query busy time

--- drivers/cpufreq/cpufreq_blu_active.c
+++ drivers/cpufreq/cpufreq_blu_active.c
@@ -52,7 +52,6 @@ struct cpufreq_blu_active_policyinfo {
 	bool reject_notification;
 	int governor_enabled;
 	struct cpufreq_blu_active_tunables *cached_tunables;
-	unsigned long *cpu_busy_times;
 };
 
 /* Protected by per-policy load_lock */
@@ -424,7 +423,7 @@ static void __cpufreq_blu_active_timer(unsigned long data, bool is_notif)
 	unsigned int index;
 	unsigned long flags;
 	unsigned long max_cpu;
-	int i, fcpu;
+	int i;
 	struct cpufreq_govinfo govinfo;
 	bool skip_hispeed_logic, skip_min_sample_time;
 	bool policy_max_fast_restore = false;
@@ -434,20 +433,16 @@ static void __cpufreq_blu_active_timer(unsigned long data, bool is_notif)
 	if (!ppol->governor_enabled)
 		goto exit;
 
-	fcpu = cpumask_first(ppol->policy->related_cpus);
 	now = ktime_to_us(ktime_get());
 	spin_lock_irqsave(&ppol->load_lock, flags);
 	ppol->last_evaluated_jiffy = get_jiffies_64();
 
-	if (tunables->use_sched_load)
-		sched_get_cpus_busy(ppol->cpu_busy_times,
-				    ppol->policy->related_cpus);
 	max_cpu = cpumask_first(ppol->policy->cpus);
 	for_each_cpu(i, ppol->policy->cpus) {
 		pcpu = &per_cpu(cpuinfo, i);
 		if (tunables->use_sched_load) {
-			cputime_speedadj = (u64)ppol->cpu_busy_times[i - fcpu]
-					* ppol->policy->cpuinfo.max_freq;
+			cputime_speedadj = (u64)sched_get_busy(i) *
+				ppol->policy->cpuinfo.max_freq;
 			do_div(cputime_speedadj, tunables->timer_rate);
 		} else {
 			now = update_load(i);
@@ -1426,7 +1421,6 @@ static struct cpufreq_blu_active_policyinfo *get_policyinfo(
 	struct cpufreq_blu_active_policyinfo *ppol =
 				per_cpu(polinfo, policy->cpu);
 	int i;
-	unsigned long *busy;
 
 	/* polinfo already allocated for policy, return */
 	if (ppol)
@@ -1435,14 +1429,6 @@ static struct cpufreq_blu_active_policyinfo *get_policyinfo(
 	ppol = kzalloc(sizeof(*ppol), GFP_KERNEL);
 	if (!ppol)
 		return ERR_PTR(-ENOMEM);
-
-	busy = kcalloc(cpumask_weight(policy->related_cpus), sizeof(*busy),
-		       GFP_KERNEL);
-	if (!busy) {
-		kfree(ppol);
-		return ERR_PTR(-ENOMEM);
-	}
-	ppol->cpu_busy_times = busy;
 
 	init_timer_deferrable(&ppol->policy_timer);
 	ppol->policy_timer.function = cpufreq_blu_active_timer;
@@ -1470,7 +1456,6 @@ static void free_policyinfo(int cpu)
 		if (per_cpu(polinfo, j) == ppol)
 			per_cpu(polinfo, cpu) = NULL;
 	kfree(ppol->cached_tunables);
-	kfree(ppol->cpu_busy_times);
 	kfree(ppol);
 }
 
