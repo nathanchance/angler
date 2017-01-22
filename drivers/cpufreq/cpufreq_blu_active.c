f79ef8650f1d346ef835e92e81f9a130d1fe2f34
jollaman999 <admin@jollaman999.com>
2017-01-22 17:26:27 +0900

    cpufreq: blu_active: Revert "Use wake_up_process_no_notif to wake up tasks"

--- drivers/cpufreq/cpufreq_blu_active.c
+++ drivers/cpufreq/cpufreq_blu_active.c
@@ -581,7 +581,7 @@ static void __cpufreq_blu_active_timer(unsigned long data, bool is_notif)
 	spin_lock_irqsave(&speedchange_cpumask_lock, flags);
 	cpumask_set_cpu(max_cpu, &speedchange_cpumask);
 	spin_unlock_irqrestore(&speedchange_cpumask_lock, flags);
-	wake_up_process_no_notif(speedchange_task);
+	wake_up_process(speedchange_task);
 
 rearm:
 	if (!timer_pending(&ppol->policy_timer))
@@ -683,7 +683,7 @@ static void cpufreq_blu_active_boost(struct cpufreq_blu_active_tunables *tunable
 	spin_unlock_irqrestore(&speedchange_cpumask_lock, flags[0]);
 
 	if (anyboost)
-		wake_up_process_no_notif(speedchange_task);
+		wake_up_process(speedchange_task);
 }
 
 static int load_change_callback(struct notifier_block *nb, unsigned long val,
@@ -1653,7 +1653,7 @@ static int __init cpufreq_blu_active_init(void)
 	get_task_struct(speedchange_task);
 
 	/* NB: wake up so the thread does not look hung to the freezer */
-	wake_up_process_no_notif(speedchange_task);
+	wake_up_process(speedchange_task);
 
 	return cpufreq_register_governor(&cpufreq_gov_blu_active);
 }
