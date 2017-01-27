8317600aca82d00327a465456b99fdee9702a299
Nathan Chancellor <natechancellor@gmail.com>
2017-01-26 21:59:50 -0700

    drivers: power: smb1351: fix maybe-uninitialized warning
    drivers/power/smb1351-charger.c: In function 'smb1351_chg_adc_notification':
    drivers/power/smb1351-charger.c:1719:9: warning: 'cur' may be used uninitialized in this function [-Wmaybe-uninitialized]
      if (cur->batt_hot ^ chip->batt_hot ||
          ~~~^~~~~~~~~~
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/power/smb1351-charger.c
+++ drivers/power/smb1351-charger.c
@@ -1611,7 +1611,7 @@ static void smb1351_chg_set_appropriate_vddmax(struct smb1351_charger *chip)
 static void smb1351_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 {
 	struct smb1351_charger *chip = ctx;
-	struct battery_status *cur;
+	struct battery_status *cur = NULL;
 	int temp;
 
 	if (state >= ADC_TM_STATE_NUM) {
