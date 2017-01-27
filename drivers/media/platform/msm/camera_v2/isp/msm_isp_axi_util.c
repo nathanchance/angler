fb012df107d54f0dc4394a97a5bfc9ab282a9756
Nathan Chancellor <natechancellor@gmail.com>
2017-01-26 21:12:32 -0700

    drivers: media: msm: camera_v2: fix unused-variable warning
    drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c: In function 'msm_isp_axi_cfg_update':
    drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c:1085:16: warning: unused variable 'flags' [-Wunused-variable]
      unsigned long flags, flags1;
                    ^~~~~
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1082,7 +1082,7 @@ void msm_isp_axi_cfg_update(struct vfe_device *vfe_dev,
 {
 	int i, j;
 	uint32_t update_state;
-	unsigned long flags, flags1;
+	unsigned long flags = 0;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev->axi_data;
 	struct msm_vfe_axi_stream *stream_info;
 	int num_stream = 0;
@@ -1098,7 +1098,7 @@ void msm_isp_axi_cfg_update(struct vfe_device *vfe_dev,
 			!stream_info->controllable_output) ||
 			stream_info->state == AVALIABLE)
 			continue;
-		spin_lock_irqsave(&stream_info->lock, flags1);
+		spin_lock_irqsave(&stream_info->lock, flags);
 		if (stream_info->state == PAUSING) {
 			/*AXI Stopped, apply update*/
 			stream_info->state = PAUSED;
@@ -1116,7 +1116,7 @@ void msm_isp_axi_cfg_update(struct vfe_device *vfe_dev,
 				stream_info->output_format;
 			stream_info->state = ACTIVE;
 		}
-		spin_unlock_irqrestore(&stream_info->lock, flags1);
+		spin_unlock_irqrestore(&stream_info->lock, flags);
 	}
 
 	if (num_stream)
