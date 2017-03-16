6458ed73343bfa6a015b2c63ceec8fd1b0096a40
Nathan Chancellor <natechancellor@gmail.com>
2017-03-15 21:35:09 -0700

    cpufreq: add Flash governor
    Currently, this is IDENTICAL to interactive. The primary purpose of
    this governor is to allow me to set defaults on boot that people can
    get rid of by changing to interactive. There is not much of a point
    to picking this at the moment.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -464,6 +464,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_FLASH)
+extern struct cpufreq_governor cpufreq_gov_flash;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_flash)
 #endif
 
 /*********************************************************************
