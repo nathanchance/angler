94d39e894246ad5bd8db30224e14eec399537c0a
Chao Yu <yuchao0@huawei.com>
2017-03-27 18:14:05 +0800

    f2fs: shrink blk plug region
    Don't use blk plug covering area where there won't be any IOs being issued.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -991,9 +991,8 @@ repeat:
 	if (kthread_should_stop())
 		return 0;
 
-	blk_start_plug(&plug);
-
 	mutex_lock(&dcc->cmd_lock);
+	blk_start_plug(&plug);
 	list_for_each_entry_safe(dc, tmp, pend_list, list) {
 		f2fs_bug_on(sbi, dc->state != D_PREP);
 
@@ -1003,6 +1002,7 @@ repeat:
 		if (iter++ > DISCARD_ISSUE_RATE)
 			break;
 	}
+	blk_finish_plug(&plug);
 
 	list_for_each_entry_safe(dc, tmp, wait_list, list) {
 		if (dc->state == D_DONE) {
@@ -1011,8 +1011,6 @@ repeat:
 		}
 	}
 	mutex_unlock(&dcc->cmd_lock);
-
-	blk_finish_plug(&plug);
 
 	iter = 0;
 	congestion_wait(BLK_RW_SYNC, HZ/50);
