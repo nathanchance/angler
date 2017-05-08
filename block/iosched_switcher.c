bd8ea40d56eefab0316dc91e95e9bfb928ecaede
Dorimanx <yuri@bynet.co.il>
2017-05-08 13:24:02 +0300

    block: I/O scheduler driver mod. delay setting noop when screen goes off.
    *Allow to delay setting noop when screen goes OFF. no point to set noop
    If user turn screen off and turn it On after 2sec to do something else!
    
    *Set dealy to 5sec, 10 is overkill.
    
    *Make sure the work is canceled ONLY if was actually active!

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -20,7 +20,7 @@
 #include <linux/state_notifier.h>
 
 #define NOOP_IOSCHED "noop"
-#define RESTORE_DELAY_MS (10000)
+#define RESTORE_DELAY_MS (5000)
 
 struct req_queue_data {
 	struct list_head list;
@@ -30,6 +30,7 @@ struct req_queue_data {
 };
 
 static struct delayed_work restore_prev;
+static struct delayed_work sleep_sched;
 static DEFINE_SPINLOCK(init_lock);
 static struct req_queue_data req_queues = {
 	.list = LIST_HEAD_INIT(req_queues.list),
@@ -71,6 +72,8 @@ static int state_notifier_callback(struct notifier_block *this,
 			 * Switch back from noop to the original iosched after a delay
 			 * when the screen is turned on.
 			 */
+			if (delayed_work_pending(&sleep_sched))
+				cancel_delayed_work_sync(&sleep_sched);
 			schedule_delayed_work(&restore_prev,
 				msecs_to_jiffies(RESTORE_DELAY_MS));
 			break;
@@ -80,8 +83,10 @@ static int state_notifier_callback(struct notifier_block *this,
 			 * the state notifier chain call in case weird things can happen
 			 * when switching elevators while the screen is off.
 			 */
-			cancel_delayed_work_sync(&restore_prev);
-			change_all_elevators(&req_queues.list, true);
+			if (delayed_work_pending(&restore_prev))
+				cancel_delayed_work_sync(&restore_prev);
+			schedule_delayed_work(&sleep_sched,
+				msecs_to_jiffies(RESTORE_DELAY_MS));
 			break;
 		default:
 			break;
@@ -93,6 +98,11 @@ static int state_notifier_callback(struct notifier_block *this,
 static void restore_prev_fn(struct work_struct *work)
 {
 	change_all_elevators(&req_queues.list, false);
+}
+
+static void set_sleep_sched_fn(struct work_struct *work)
+{
+	change_all_elevators(&req_queues.list, true);
 }
 
 int init_iosched_switcher(struct request_queue *q)
@@ -117,6 +127,7 @@ static int iosched_switcher_core_init(void)
 	int ret = 0;
 
 	INIT_DELAYED_WORK(&restore_prev, restore_prev_fn);
+	INIT_DELAYED_WORK(&sleep_sched, set_sleep_sched_fn);
 	notif.notifier_call = state_notifier_callback;
 	ret = state_register_client(&notif);
 
