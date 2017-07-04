b0b654cb86af7d4f03f0e395c9de4fffc16b3c26
Nathan Chancellor <natechancellor@gmail.com>
2017-07-03 22:23:05 -0700

    Prepare to force disable touchboost
    The gaming profile is more powerful than this in my opinion and the
    msm_perf driver is just terrible so we're going to neuter everything.
    
    Revert "msm_performance: disable touchboost by default"
    This reverts commit b9d94f0795733145e0d0e14e30253f3c67a59f1e.
    
    Revert "msm_performance: Make input boosting optional"
    This reverts commit 56e1556cae9fb8d08ce50d64c4a7f7b581391d83.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -25,8 +25,6 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 
-static int touchboost = 0;
-
 static struct mutex managed_cpus_lock;
 
 /* Maximum number to clusters that this module will manage*/
@@ -140,29 +138,6 @@ static struct task_struct *notify_thread;
 #define LAST_LD_CHECK_TOL	(2 * USEC_PER_MSEC)
 
 /**************************sysfs start********************************/
-
-static int set_touchboost(const char *buf, const struct kernel_param *kp)
-{
-	int val;
-
-	if (sscanf(buf, "%d\n", &val) != 1)
-		return -EINVAL;
-
-	touchboost = val;
-
-	return 0;
-}
-
-static int get_touchboost(char *buf, const struct kernel_param *kp)
-{
-	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
-}
-
-static const struct kernel_param_ops param_ops_touchboost = {
-	.set = set_touchboost,
-	.get = get_touchboost,
-};
-device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 
 static int set_num_clusters(const char *buf, const struct kernel_param *kp)
 {
@@ -354,9 +329,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	cpumask_var_t limit_mask;
 	int ret;
 
-	if (!touchboost)
-		return 0;
-
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -439,9 +411,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
-
-	if (!touchboost)
-		return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
